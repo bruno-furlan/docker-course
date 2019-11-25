1f0fe468bb69ec196de747b809a7e159fa20dfc9
Bruno Furlan <bruno.furlan@dpaschoal.com.br>
2019-11-25 20:49:00 -0300

    change app text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           8=================>>
+          Feature Branch
         </p>
         <a
           className="App-link"
